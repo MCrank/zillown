7ce276e3df05cd4c8f9125ef965ba84f3436ca46
MCrank <marco_mcse@hotmail.com>
2018-12-11 21:04:09 -0600

    added MyNavbar

---
+++ src/components/MyNavbar/MyNavbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink,
+} from 'reactstrap';
+import './MyNavbar.scss';
+
+class MyNavbar extends React.Component {
+  state = {
+    isOpen: false,
+  };
+
+  render() {
+    return (
+      <div className="MyNavbar">
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/">Zillown</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Logout</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default MyNavbar;
