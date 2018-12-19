54caefd102eac4be336df85c15d061b283635915
MCrank <marco_mcse@hotmail.com>
2018-12-18 19:20:07 -0600

    added address form

--- src/components/ListingForm/ListingForm.js
+++ src/components/ListingForm/ListingForm.js
@@ -5,7 +5,20 @@ class ListingForm extends React.Component {
   render() {
     return (
       <div className="ListingForm col">
-        <h2>ListingForm</h2>
+        <h2>Add New Listing</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              className="form-control"
+              id="address"
+              aria-describedby="addressHelp"
+              placeholder="123 United Way, Hollywood, CA 90210"
+            />
+          </div>
+          <button className="btn btn-secondary">Save Listing</button>
+        </form>
       </div>
     );
   }
