f288a3efc11f4d2b08ebc0cecb98911d7215403d
MCrank <marco_mcse@hotmail.com>
2018-12-18 19:48:38 -0600

    update state

--- src/components/ListingForm/ListingForm.js
+++ src/components/ListingForm/ListingForm.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import './ListingForm.scss';
 
+const defaultListing = {
+  address: '',
+  squareFootage: 0,
+  price: 0,
+  numBeds: 0,
+  numBaths: 0,
+  heating: '',
+  cooling: '',
+  imageUrl: '',
+  uid: '',
+};
+
 class ListingForm extends React.Component {
+  state = {
+    newListing: defaultListing,
+  };
+
+  formFieldStringState = (name, event) => {
+    event.preventDefault();
+    const tempListing = { ...this.state.newListing };
+    tempListing[name] = event.target.value;
+    this.setState({ newListing: tempListing });
+  };
+
+  // formFieldNumberState = (name, event) => {
+  //   const tempListing = { ...this.state.newListing };
+  //   tempListing[name] = event.target.value * 1;
+  //   this.setState({ newListing: tempListing });
+  // };
+
+  addressChange = (event) => {
+    this.formFieldStringState('address', event);
+  };
+
   render() {
+    const { newListing } = this.state;
     return (
       <div className="ListingForm col">
         <h2>Add New Listing</h2>
@@ -15,6 +49,8 @@ class ListingForm extends React.Component {
               id="address"
               aria-describedby="addressHelp"
               placeholder="123 United Way, Hollywood, CA 90210"
+              value={newListing.address}
+              onChange={this.addressChange}
             />
           </div>
           <button className="btn btn-secondary">Save Listing</button>
